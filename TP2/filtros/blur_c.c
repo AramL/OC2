9c6e78d7c5605a8ff19f372b0f7615b440f2eed2
Leandro Javier <leandrojavr@gmail.com>
2015-09-09 03:50:14 -0300

    blur kernel

--- TP2/filtros/blur_c.c
+++ TP2/filtros/blur_c.c
@@ -4,21 +4,21 @@
 #include "../tp2.h"
 
 short kernel(short x, short y, float sigma){
-	short pi =  arcsin(90)*2;
-	x = x*x;
-	y = y*y;
-	float sigmasq = sigma*sigma;
-	short ret = exp(-(x+y)/(2*sigmasq)/(2* pi * sigmasq)
+    short pi =  arcsin(90)*2;
+    x = x*x;
+    y = y*y;
+    float sigmasq = sigma*sigma;
+    short ret = exp(-(x+y)/(2*sigmasq)/(2* pi * sigmasq)
 }
 
-void blur_c    (unsigned char *src, unsigned char *dst, int cols, int filas, float sigma, int radius){	
+void blur_c    (unsigned char *src, unsigned char *dst, int cols, int filas, float sigma, int radius){  
     unsigned char (*src_matrix)[cols*4] = (unsigned char (*)[cols*4]) src;
     unsigned char (*dst_matrix)[cols*4] = (unsigned char (*)[cols*4]) dst;
 
     /** Por el enunciado no hace falta operar sobre los bordes de la imagen **/
     for(int x = 1; x < n-1; x++){
         for(int y = 1; y < m-1; y++){
-     		dst_matrix[x][y*4] = inf;                           //blue
+            dst_matrix[x][y*4] = inf;                           //blue
             dst_matrix[x][y*4+1] = inf;                     //red
             dst_matrix[x][y*4+2] = inf;                     //green
             //dst_matrix[x][y*4+3]= 255;                      //alpha
