1f8abb03e38a432209d85a440dc62d68f002635f
Leandro Javier <leandrojavr@gmail.com>
2015-09-09 20:23:41 -0300

    blur

--- TP2/filtros/blur_c.c
+++ TP2/filtros/blur_c.c
@@ -3,25 +3,38 @@
 #include <math.h>
 #include "../tp2.h"
 
-short kernel(short x, short y, float sigma){
-    short pi =  arcsin(90)*2;
+
+float pi = asin(0.5) * 6;
+
+float Kernel(short x, short y, float sigma){
     x = x*x;
     y = y*y;
     float sigmasq = sigma*sigma;
-    short ret = exp(-(x+y)/(2*sigmasq)/(2* pi * sigmasq)
+    float ret = exp((-1)*(x+y)/(2*sigmasq))/(2* pi * sigmasq);
+
+    return ret;        
 }
 
 void blur_c    (unsigned char *src, unsigned char *dst, int cols, int filas, float sigma, int radius){  
     unsigned char (*src_matrix)[cols*4] = (unsigned char (*)[cols*4]) src;
     unsigned char (*dst_matrix)[cols*4] = (unsigned char (*)[cols*4]) dst;
 
-    /** Por el enunciado no hace falta operar sobre los bordes de la imagen **/
-    for(int x = 1; x < n-1; x++){
-        for(int y = 1; y < m-1; y++){
-            dst_matrix[x][y*4] = inf;                           //blue
-            dst_matrix[x][y*4+1] = inf;                     //red
-            dst_matrix[x][y*4+2] = inf;                     //green
-            //dst_matrix[x][y*4+3]= 255;                      //alpha
+    for(int y = 1; y < cols-1; y++){
+        for(int x = 1; x < filas-1; x++){
+            float tempB = 0;
+            float tempR = 0;
+            float tempG = 0;
+            for(int j = -radius; j <= radius; j++){
+                for(int k = -radius; k<= radius; k++){
+                    tempB += src_matrix[y+j][(x+k)*4] * Kernel(radius - j, radius - k, sigma);
+                    tempR += src_matrix[y+j][(x+k)*4+1] * Kernel(radius - j, radius - k, sigma);
+                    tempG += src_matrix[y+j][(x+k)*4+1] * Kernel(radius - j, radius - k, sigma);
+                }
+            }
+            dst_matrix[y][x*4] = tempB;
+            dst_matrix[y][x*4+1] = tempR;
+            dst_matrix[y][x*4+2] = tempG;
+            dst_matrix[x][x*4+3]= 255;                      //alpha
         }
     }
-}
+ }
