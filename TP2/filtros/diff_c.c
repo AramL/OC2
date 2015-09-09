587ffdbe4352d1a3374acef27bcf56af16f702fb
Leandro Javier <leandrojavr@gmail.com>
2015-09-09 00:40:17 -0300

    esqueleto del informe

--- TP2/filtros/diff_c.c
+++ TP2/filtros/diff_c.c
@@ -7,5 +7,4 @@ void diff_c (unsigned char *src, unsigned char *src_2, unsigned char *dst, int m
 	unsigned char (*src_matrix)[src_row_size] = (unsigned char (*)[src_row_size]) src;
 	unsigned char (*src_2_matrix)[src_2_row_size] = (unsigned char (*)[src_2_row_size]) src_2;
 	unsigned char (*dst_matrix)[dst_row_size] = (unsigned char (*)[dst_row_size]) dst;
-
 }
