9295a7a40f22de15d23fc9c2208d197e62aa83dd
Leandro Javier <leandrojavr@gmail.com>
2015-09-09 23:53:45 -0300

    removi la llamada a la función Kernel

--- TP2/filtros/blur_c.c
+++ TP2/filtros/blur_c.c
@@ -4,31 +4,29 @@
 #include "../tp2.h"
 
 
-float pi = asin(0.5) * 6;
+//float pi = asin(0.5) * 6;
 
-float Kernel(short x, short y, float sigma){
-    x = x*x;
-    y = y*y;
-    float sigmasq = sigma*sigma;
-    float ret = exp((x+y)/((-2)*sigmasq))/(2* pi * sigmasq);
+#define pi 3.14159265359
 
-    return ret;        
-}
 
 void blur_c    (unsigned char *src, unsigned char *dst, int cols, int filas, float sigma, int radius){  
     unsigned char (*src_matrix)[cols*4] = (unsigned char (*)[cols*4]) src;
     unsigned char (*dst_matrix)[cols*4] = (unsigned char (*)[cols*4]) dst;
+
+    float sigmasq = sigma*sigma;
 
     for(int y = radius; y < cols-radius; y++){
         for(int x = radius; x < filas-radius; x++){
             float tempB = 0;
             float tempR = 0;
             float tempG = 0;
+            float tempK = 0;
             for(int j = -radius; j <= radius; j++){
                 for(int k = -radius; k<= radius; k++){
-                    tempB += src_matrix[y+j][(x+k)*4] * Kernel(radius - j, radius - k, sigma);
-                    tempR += src_matrix[y+j][(x+k)*4+1] * Kernel(radius - j, radius - k, sigma);
-                    tempG += src_matrix[y+j][(x+k)*4+2] * Kernel(radius - j, radius - k, sigma);
+                    tempK = exp((((radius - j)*(radius - j))+((radius - k)*(radius - k)))/((-2)*sigmasq))/(2* pi * sigmasq);
+                    tempB += src_matrix[y+j][(x+k)*4] * tempK;
+                    tempR += src_matrix[y+j][(x+k)*4+1] * tempK;
+                    tempG += src_matrix[y+j][(x+k)*4+2] * tempK;
                 }
             }
             dst_matrix[y][x*4] = (unsigned char)tempB;
