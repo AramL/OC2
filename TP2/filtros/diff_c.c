dda49079ddd09e81a7913936c2abcb802fc82770
AramL <leandrojavr@gmail.com>
2015-09-21 20:59:11 -0300

    diferencia en C con loop unrolling

--- TP2/filtros/diff_c.c
+++ TP2/filtros/diff_c.c
@@ -21,21 +21,36 @@ void diff_c (unsigned char *src, unsigned char *src_2, unsigned char *dst, int m
     unsigned char (*src_2_matrix)[src_2_row_size]   = (unsigned char (*)[src_2_row_size]) src_2;
     unsigned char (*dst_matrix)[dst_row_size]       = (unsigned char (*)[dst_row_size]) dst;
 
-
+    unsigned char n1,n2,n3,n4;
     for(register int y = 0; y < n; y++){
-        for(register int x = 0; x < m; x++){
-            unsigned char inf = 
-                       infNorm(src_matrix[y][x*4], src_matrix[y][x*4+1], src_matrix[y][x*4+2],
-                                src_2_matrix[y][x*4], src_2_matrix[y][x*4+1], src_2_matrix[y][x*4+2]);
-            dst_matrix[y][x*4]   = inf;                     //blue
-            dst_matrix[y][x*4+1] = inf;                     //red
-            dst_matrix[y][x*4+2] = inf;                     //green
+        for(register int x = 0; x < m; x=x+4){
+            n1 = infNorm(src_matrix[y][x*4], src_matrix[y][x*4+1], src_matrix[y][x*4+2],
+                        src_2_matrix[y][x*4], src_2_matrix[y][x*4+1], src_2_matrix[y][x*4+2]);
+            n2 = infNorm(src_matrix[y][x*4+4], src_matrix[y][x*4+5], src_matrix[y][x*4+6],
+                        src_2_matrix[y][x*4+4], src_2_matrix[y][x*4+5], src_2_matrix[y][x*4+6]);
+            n3 = infNorm(src_matrix[y][x*4+8], src_matrix[y][x*4+9], src_matrix[y][x*4+10],
+                        src_2_matrix[y][x*4+8], src_2_matrix[y][x*4+9], src_2_matrix[y][x*4+10]);
+            n4 = infNorm(src_matrix[y][x*4+12], src_matrix[y][x*4+13], src_matrix[y][x*4+14],
+                        src_2_matrix[y][x*4+12], src_2_matrix[y][x*4+13], src_2_matrix[y][x*4+14]);
+
+            dst_matrix[y][x*4]   = n1;                      //blue
+            dst_matrix[y][x*4+1] = n1;                      //red
+            dst_matrix[y][x*4+2] = n1;                      //green
             dst_matrix[y][x*4+3] = 255;                     //alpha
+            dst_matrix[y][x*4+4] = n2;                      //blue
+            dst_matrix[y][x*4+5] = n2;                      //red
+            dst_matrix[y][x*4+6] = n2;                      //green
+            dst_matrix[y][x*4+7] = 255;                     //alpha
+            dst_matrix[y][x*4+8] = n3;                      //blue
+            dst_matrix[y][x*4+9] = n3;                      //red
+            dst_matrix[y][x*4+10] = n3;                      //green
+            dst_matrix[y][x*4+11] = 255;                     //alpha
+            dst_matrix[y][x*4+12] = n4;                      //blue
+            dst_matrix[y][x*4+13] = n4;                      //red
+            dst_matrix[y][x*4+14] = n4;                      //green
+            dst_matrix[y][x*4+15] = 255;                     //alpha
+
 
         }
     }
 }
-
-
-
-
