bae2135d41f089f3b7285ba626e85dabe2e7be10
AramL <leandrojavr@gmail.com>
2015-09-21 14:32:38 -0300

    desviacion mediana y promedio

--- TP2/python/graficar.py
+++ TP2/python/graficar.py
@@ -4,18 +4,19 @@ import numpy as np
 
 
 
-def levantar(filename, titulo, yaxis, nombre1, nombre2):
-  with open(filename, 'r') as f:
+def levantar(filename, titulo, yaxis, nombre1, nombre2):  #nombre1,2 son los nombres de las leyenda de las barras
+  with open(filename, 'r') as f:                          #titulo es el titulo del grafico e yaxis es el nombre del eje 'y'
     comp1 = []
     comp1 = [int(x) for x in f.readline().split()]
     comp2 = []
     comp2 = [int(x) for x in f.readline().split()]
-  graficar(filename, titulo, yaxis, nombre1, nombre2, comp1,comp2 )
+  graficarMediana(filename, titulo, yaxis, nombre1, nombre2, comp1,comp2 )
+  graficarDesviacion(filename, titulo, yaxis, nombre1, nombre2, comp1, comp2)
 
-
-def graficar(filename, titulo, yaxis, nombre1, nombre2, arg1, arg2,):
+def graficarPromedio(filename, titulo, yaxis, nombre1, nombre2, arg1, arg2,):
   plt.switch_backend('Qt4Agg')
   fig = plt.figure(figsize=(10,6))
+  #fig.set_size_inches(6,6)
   ax = fig.add_subplot(1,2,1)
   arg1mean = np.mean(arg1)
   arg2mean = np.mean(arg2)
@@ -24,14 +25,58 @@ def graficar(filename, titulo, yaxis, nombre1, nombre2, arg1, arg2,):
   arr.append(arg2mean)
   ax.bar([1,2], arr, align='center', color=['b','r'])
   plt.title(titulo)
-  #plt.tick_params(axis='x', which='both', bottom='off', top='off', labelbottom='off')
+  #plt.tick_params(axis='x', which='both', bottom='off', top='off', labelbottom='off')  <- esto es por si quieren sacar los labels de x
   labels = []
   labels.append(nombre1)
   labels.append(nombre2)
-  plt.ylabel(yaxis)
+  plt.ylabel(yaxis+' (promedio)')
   plt.xticks([1,2], labels)
 
-  plt.savefig(filename+'.png')
+  plt.savefig(filename+' (promedio).png', bbox_inches = 'tight')
+  plt.xticks()
+
+def graficarDesviacion(filename, titulo, yaxis, nombre1, nombre2, arg1, arg2):
+  plt.switch_backend('Qt4Agg')
+  fig = plt.figure(figsize=(10,6))
+  #fig.set_size_inches(6,6)
+  ax = fig.add_subplot(1,2,1)
+  arg1mean = np.std(arg1)
+  arg2mean = np.std(arg2)
+  arr = []
+  arr.append(arg1mean)
+  arr.append(arg2mean)
+  ax.bar([1,2], arr, align='center', color=['b','r'])
+  plt.title(titulo)
+  #plt.tick_params(axis='x', which='both', bottom='off', top='off', labelbottom='off')  <- esto es por si quieren sacar los labels de x
+  labels = []
+  labels.append(nombre1)
+  labels.append(nombre2)
+  plt.ylabel(yaxis +' (desviacion)' )
+  plt.xticks([1,2], labels)
+
+  plt.savefig(filename +' desviacion.png', bbox_inches = 'tight')
+  plt.xticks()
+
+def graficarMediana(filename, titulo, yaxis, nombre1, nombre2, arg1, arg2):
+  plt.switch_backend('Qt4Agg')
+  fig = plt.figure(figsize=(10,6))
+  #fig.set_size_inches(6,6)
+  ax = fig.add_subplot(1,2,1)
+  arg1mean = np.median(arg1)
+  arg2mean = np.median(arg2)
+  arr = []
+  arr.append(arg1mean)
+  arr.append(arg2mean)
+  ax.bar([1,2], arr, align='center', color=['b','r'])
+  plt.title(titulo)
+  #plt.tick_params(axis='x', which='both', bottom='off', top='off', labelbottom='off')  <- esto es por si quieren sacar los labels de x
+  labels = []
+  labels.append(nombre1)
+  labels.append(nombre2)
+  plt.ylabel(yaxis +' (mediana)' )
+  plt.xticks([1,2], labels)
+
+  plt.savefig(filename +' mediana.png', bbox_inches = 'tight')
   plt.xticks()
 
 if __name__ == "__main__":
