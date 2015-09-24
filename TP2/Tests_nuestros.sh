4e028466880927953083fce9a264941b5a444282
AramL <leandrojavr@gmail.com>
2015-09-24 20:55:22 -0300

    :D

--- TP2/Tests_nuestros.sh
+++ TP2/Tests_nuestros.sh
@@ -15,25 +15,24 @@ fi
 echo " "
 echo "**Corriendo mediciones ASM vs C"
 
-for j in 1 ..100
+for j in {1 2 3 4 5 6 7 8 9 10}
 do
-  ./build/tp2 -i c diff img/scene0.bmp img/scene400.bmp | cut -d':' -f2 | sed '10,10!d' | xargs echo -n | tee -a testASM_C
-  echo -n " " >> testASM_C
-  echo -n j 
+  ./build/tp2 -i asm diff img/scene0.bmp img/scene400.bmp | cut -d':' -f2 | sed '10,10!d' | xargs echo -n | tee -a ./python/testASM_C
+  echo -n " " >> ./python/testASM_C
 done
 
-echo " " >> testASM_C
+echo "" >> ./python/testASM_C
 
-for j in 1 .. 100
+for r in {1 2 3 4 5 6 7 8 9 10}
 do
-  ./build/tp2 -i c diff img/scene0.bmp img/scene400.bmp | cut -d':' -f2 | sed '10,10!d' | xargs echo -n | tee -a testASM_C
-  echo -n " " >> testASM_C
+  ./build/tp2 -i c diff img/scene0.bmp img/scene400.bmp | cut -d':' -f2 | sed '10,10!d' | xargs echo -n | tee -a ./python/testASM_C
+  echo -n " " >> ./python/testASM_C
 done
 
 echo " "
 echo "**graficando"
 
-./python/graficar.py "compare_two" testASM_C "C vs ASM" "CPU Ticks"  "Assembly" "gcc" "lpm" 
+./python/graficar.py "compare_two" testASM_C "C vs ASM" "CPU Ticks"  Assembly gcc a 
 
 echo " "
 echo "gracias uriel :D"
