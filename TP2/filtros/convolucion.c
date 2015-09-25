90a62b6d7d5c0b315dd1e21309e9233562b52077
AramL <leandrojavr@gmail.com>
2015-09-25 00:21:56 -0300

    Update convolucion.c

--- TP2/filtros/convolucion.c
+++ TP2/filtros/convolucion.c
@@ -10,8 +10,7 @@ float* matrizDeConvolucion(float sigma, int radius){
 
     float sigmasq = sigma*sigma;
 
-    float *convolutionMatrix = (float *) malloc(sizeof(float) * ((2*radius)+1) * ((2*radius)+1) );
-    //Aca antes estaba unsigned char y se volvia todo loco, PONERLO EN EL INFORMEEEEE!#!!!!!#$!"&$!"=/(#)=
+    float *convolutionMatrix = (float *) malloc(sizeof(float) * ((2*radius)+1) * ((2*radius)+1));
     for(int h = -radius; h <= radius; h++)
          for(int p = -radius; p <= radius; p++)
               convolutionMatrix[(h+radius)*(2*radius+1) + p+radius]= exp((((radius - h)*(radius - h))+((radius - p)*(radius - p)))/((-2)*sigmasq))/(2* pi * sigmasq);
