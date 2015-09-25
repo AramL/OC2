00e84bc4f134ba2988b736e8714e7c126bc4c919
maxiferwort <maxifwortman@gmail.com>
2015-09-25 18:49:42 -0300

    blur que itera

--- TP2/filtros/blur_asm.asm
+++ TP2/filtros/blur_asm.asm
@@ -10,77 +10,189 @@ section .data
 section .text
 ;void blur_asm    (unsigned char *src,unsigned char *dst,int filas,int cols,float sigma,
     ;int radius)
-
+;r12 va a estar la matrix creada en c++
 ;_blur_asm:
 blur_asm:
 
+	push	rbp
+	mov	rbp,rsp
+	push	rbx
+	push	r12
+	push	r13
+	push	r14
+	push	r15
+	sub	rsp,8
+
+
+
 ;rdi=src rsi = dst rdx = filas rcx = cols r8 = radius xmm0 = sigma
+		;mov rsp,rcx
+;r15 = x de la posicion calculando actual	
+	xor r15,r15
+	mov r15d,r8d
+;r14 es la posicion y de nuestro calculando actual
+	xor r14,r14
+	mov r14d,r8d 
+	mov r13,r8
+	shl r13,1
+	inc r13 ;diametro
+	mov rbx,rdx ;filas
+	mov r13,rcx ; columnas
+.principio:
+	mov r12,rdi ;r12 va a ser el puntero a la matriz original
+	mov rcx,r14 ;posicion en Y
+	sub rcx,r8 ;radio
+	xor rax,rax
+	mov eax,r8d ;radio
+	mul rcx
+	add rax,r15
+	sub rax,r8 ;le resto el radio 
+	shl rax,2 ;multiplico por 4
+	add r12,rax ;r12 es el puntero a primer valor
+	;mov r5, rax editar esto no tengo convolucion todavia
+	pxor xmm2,xmm2
+	cvtdq2ps xmm2,xmm2 ;esto es un 0 float
+	xor r11,r11 ; va a ser el puntero a convolucion x
+	xor r10,r10 ;va a ser el contador a convolucion y
+.empezarACalcular:
+	
+	
+	
+	
+	
+	;movdqu xmm0,[r12] ;primer original
+	;movdqu xmm1,r9  ;valor conv todavia no tengo conv
+ 	;desempaqueto y multiplico
+	addps xmm2,xmm4 ;agrego el primer valor
+	inc r11 ;incremento puntero en x
+	mov rcx,r8
+	shl rcx,1
+	inc rcx ;diametro
+	cmp r11,rcx ;comparo con diametro
+	jne .noLLegoAlDiametroEnElPrimerNodo
+	inc r10 ;incremento puntero a y
+	cmp r10,rcx ;comparo con diametro
+	je .termine
+	xor rax,rax ;agrego un pixel a la matriz de conv
+	inc rax
+	shl rax,2
+	add r9,rax ;agregue un pixel a la matriz conv (proximo)
+	mov rax,rdx ;muevo columnas a r9
+	sub rax,rcx ;le resto el diametro
+	shl rax,2
+	add r12,rax ; voy al proximo valor de la matriz original
+	xor r11,r11
+	jmp .empezarACalcular
+.noLLegoAlDiametroEnElPrimerNodo:	
+	addps xmm2,xmm5 ;agrego el segundo valor
+	inc r11 ;incremento puntero en x
+	cmp r11,rcx ;comparo con diametro
+	jne .noLLegoAlDiametroEnElSegundoNodo
+	inc r10 ;incremento puntero a y
+	cmp r10,rcx ;comparo con diametro
+	je .termine
+	xor rax,rax ;agrego un pixel a la matriz de conv
+	inc rax
+	inc rax
+	shl rax,2
+	add r9,rax ;agregue un dos a la matriz conv (proximo)
+	mov rax,rdx ;muevo columnas a r9
+	sub rax,rcx ;le resto el diametro
+	shl rax,2
+	inc rax 
+	add r12,rax ; voy al proximo valor de la matriz original
+	xor r11,r11
+	jmp .empezarACalcular
+.noLLegoAlDiametroEnElSegundoNodo:
+	addps xmm2,xmm6 ;agrego el primer valor
+	inc r11 ;incremento puntero en x
+	cmp r11,rcx ;comparo con diametro
+	jne .noLLegoAlDiametroEnElTercerNodo
+	inc r10 ;incremento puntero a y
+	cmp r10,rcx ;comparo con diametro
+	je .termine
+	xor rax,rax ;agrego tres pixeles a la matriz de conv
+	inc rax
+	inc rax
+	inc rax
+	shl rax,2
+	add r9,rax ;agregue tres pixeles a la matriz conv (proximo)
+	mov rax,rdx ;muevo columnas a r9
+	sub rax,rcx ;le resto el diametro
+	inc rax
+	inc rax ;le agrego 2 pixeles
+	shl rax,2
+	add r12,rax ; voy al proximo valor de la matriz original
+	xor r11,r11
+	jmp .empezarACalcular
+.noLLegoAlDiametroEnElTercerNodo:
+	addps xmm2,xmm7 ;agrego el primer valor
+	inc r11 ;incremento puntero en x
+	cmp r11,rcx ;comparo con diametro
+	jne .noLLegoAlDiametroEnElCuartoNodo
+	inc r10 ;incremento puntero a y
+	cmp r10,rcx ;comparo con diametro
+	je .termine
+	xor rax,rax ;agrego cuatro pixel a la matriz de conv
+	inc rax
+	shl rax,4
+	add r9,rax ;agregue cuatro pixeles a la matriz conv (proximo)
+	mov rax,rdx ;muevo columnas a r9
+	sub rax,r13 ;le resto el diametro
+	inc rax
+	inc rax
+	inc rax
+	shl rax,2
+	add r12,rax ; voy al proximo valor de la matriz original
+	xor r11,r11
+	jmp .empezarACalcular
+.noLLegoAlDiametroEnElCuartoNodo:
+	xor rax,rax ;agrego cuatro pixel a la matriz de conv
+	inc rax
+	shl rax,4
+	add r9,rax ;agregue cuatro pixeles a la matriz conv (proximo)
+	add r12,rax
+	jmp .empezarACalcular
+.termine:
+	;pixel(punteroOriginalX,punteroOriginalY) = contadores
+	;r14 = y r15 = x
+	xor rax,rax
+	mov eax,ebx ;columnas
+	mov rcx,r14 ;posicion en y
+	mul rcx 
+	add rax,r15 ;posicion en x
+	shl rax,2 ;pos
+	mov r11,rdi ;src
+	mov r12,rsi ;dest
+	add r11,rax
+	add r12,rax
+	;----------------;
+	;mov r10d,[r11]
+	xor r10,r10
+	mov [r12],r10d
+	;----------------;
+	inc r15 ;puntero a x
+	mov rax,r13 ;columnas
+	sub rax,r8 ;radio
+	cmp r15,rax	;final
+	jne .principio
+	inc r14 ;puntero a y
+	mov r15,r8 ;radio
+ 	mov rax,rbx ;filas
+ 	sub rax,r8 ;radio
+	cmp r14,rax ;alto
+	je .terminePosta
+	;mov r15,r8 
+	jmp .principio
+.terminePosta:
 
-;primer elemento = rdi + radio * (rcx + 1)
-xor rax,rax
-mov eax,r8d
-inc rcx
-mul rcx
-shl rax,2
-add rdi,rax
-;estoy parado en el elemento primero a calcular en rdi
 
-
-;--------------------------------;
-
-
-
-
-;ciclo
-
-
-movdqu xmm0,[rdi] ;levanto 4 pixeles
-	;XMM0= [-R0-|G0|B0|A0|-R1-|G1|B1|A1|-R2-|G2|B2|A2|-R3-|G3|B3|A3]	
-	pxor   xmm11,xmm11 ;xmm11 va a ser una mascara de 0, para extender el signo	
-	;XMM11= [0000-0000-0000-0000]	
-	movdqu xmm1,xmm0  ; duplico lo que levante a xmm1
-	;XMM1= [-R0-|G0|B0|A0|-R1-|G1|B1|A1|-R2-|G2|B2|A2|-R3-|G3|B3|A3]	
-	punpcklbw xmm0,xmm11 ;muevo los bytes bajos de xmm0, y les extendiendo los 0 a dw
-	;XMM0= 0|x|0|x|----|0|x|0|x|----|0|x|0|x|----|0|x|0|x	
-	punpckhbw xmm1,xmm11 ;muevo los bytes altos de xmm1, y les extendiendo los 0 a dw
-	;XMM1= 0|x|0|x|----|0|x|0|x|----|0|x|0|x|----|0|x|0|x	
-	movdqu xmm2,xmm0 ;duplico lo que tengo en xmm0
-	;XMM2= 0|x|0|x|----|0|x|0|x|----|0|x|0|x|----|0|x|0|x	
-	punpckhwd xmm2,xmm11 ;muevo 16 bytes bajos de xmm2 y les extiendo el signo
-    ;XMM2= 0|0|0|x|----|0|0|0|x|----|0|0|0|x|----|0|0|0|x	
-    movdqu xmm3,xmm1 ;duplico lo que tengo en xmm1
-	;XMM3= 0|x|0|x|----|0|x|0|x|----|0|x|0|x|----|0|x|0|x	
-    punpckhwd xmm3,xmm11 ;muevo 16 bytes altos de xmm3 y les extiendo el signo
-    ;XMM3= 0|0|0|x|----|0|0|0|x|----|0|0|0|x|----|0|0|0|x
-    punpcklwd xmm0,xmm11 ;muevo 16 bytes bajos de xmm0 y les extiendo el signo
-    ;XMM0= 0|0|0|x|----|0|0|0|x|----|0|0|0|x|----|0|0|0|x    
-    punpcklwd xmm1,xmm11 ;muevo 16 bytes bajos de xmm1 y les extiendo el signo
-    ;XMM1= 0|0|0|x|----|0|0|0|x|----|0|0|0|x|----|0|0|0|x    
-    ;ya tengo todo el double words integers, ahora convierto a float
-    cvtdq2ps xmm0,xmm0
-    cvtdq2ps xmm1,xmm1
-    cvtdq2ps xmm2,xmm2
-    cvtdq2ps xmm3,xmm3
-    ;ahora puedo operar
-    
-    ;fin operar
-    
-    ;desempaquetar
-    
-    ;convierto de double a double word integer
-    cvttps2dq xmm0,xmm0
-    cvttps2dq xmm1,xmm1
-    cvttps2dq xmm2,xmm2
-    cvttps2dq xmm3,xmm3
-    ;convierto de dw int a w int
-    packusdw xmm0,xmm2
-    packusdw xmm1,xmm3
-    packuswb xmm0,xmm1
-    
-    movdqu [rsi],xmm0
-    
-    ;fin ciclo
-    
-
+	add	rsp,8
+	pop	r15
+	pop	r14
+	pop	r13
+	pop	r12
+	pop	rbx
+	pop	rbp
 
 	ret
