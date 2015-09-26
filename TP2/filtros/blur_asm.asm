a770d33cc7de5d0a95b68b9659c9aff0ea237bc3
Maximiliano <maxifwortman@gmail.com>
2015-09-26 18:39:02 -0300

    blur terminado

--- TP2/filtros/blur_asm.asm
+++ TP2/filtros/blur_asm.asm
@@ -26,6 +26,10 @@ blur_asm:
 	sub	rsp,8
 	
 ;rdi=src rsi = dst rdx = filas rcx = cols r8 = radius xmm0 = sigma
+;voy a hacer una unica llamada a c para conseguir la matriz
+
+	
+
 	mov r12,rdi
 	mov r13,rsi
 	mov r14,rdx
@@ -56,17 +60,6 @@ blur_asm:
 	mov rbx,rdx ;filas
 	mov r13,rcx ; columnas
 .principio:
-	mov r12,rdi ;r12 va a ser el puntero a la matriz original
-	mov rcx,r14 ;posicion en Y
-	sub rcx,r8 ;radio
-	xor rax,rax
-	mov eax,r8d ;radio
-	mul rcx
-	add rax,r15
-	sub rax,r8 ;le resto el radio 
-	shl rax,2 ;multiplico por 4
-	add r12,rax ;r12 es el puntero a primer valor
-	;mov r5, rax editar esto no tengo convolucion todavia
 	pxor xmm2,xmm2
 	cvtdq2ps xmm2,xmm2 ;esto es un 0 float
 	xor r11,r11 ; va a ser el puntero a convolucion x
@@ -74,7 +67,21 @@ blur_asm:
 	pxor xmm5,xmm5
 	cvtdq2ps xmm5,xmm5 ;esto es un 0 float
 .empezarACalcular:
-	movdqu xmm0,[r12]	
+	;Calculo la posicion a partir de la direccion de memoria de la matriz original en rdi
+	mov rcx,r14 ;posicion actual convolucion y
+	sub rcx,r8 ;radio
+	add rcx,r10 ;puntero a y
+	mov rax,r13
+	mul rcx
+	mov rcx,r15
+	sub rcx,r8
+	add rcx,r11;puntero a x
+	add rax,rcx
+	shl rax,2
+	mov r12,rdi
+	add r12,rax
+	;ya tengo la posicion aca calculada
+	movdqu xmm0,[r12]	;levanto 4 pixeles de la matriz original
 	;xmm0  = [b1 | r1 | g1 | a1 | b2 | r2 | g2 | a2 | b3 | r3 | g3 | a3 | b4 | r4 | g4 | a4]
 	pxor xmm7, xmm7
 	movdqu xmm1, xmm0
@@ -82,67 +89,57 @@ blur_asm:
 	punpcklbw xmm1, xmm7  ; [ 0 | b3 |  0  | r3 |  0  | g3 |  0 | a3 |  0 | b4 |  0 | r4 |  0 | g4 | 0 | a4 ]
 	movdqu xmm2, xmm0
 	movdqu xmm3, xmm1
-	punpckhdq xmm0, xmm7  ; [ 0 |  0 | 0 | b1 | 0 | 0 | 0 |  r1 | 0 | 0 | 0 |  g1 | 0 | 0 | 0 | a1 ] 
-	punpckldq xmm2, xmm7   ; [ 0 |  0 | 0 | b2 | 0 | 0 | 0 |  r2 | 0 | 0 | 0 |  g2 | 0 | 0 | 0 | a2 ] 
-	punpckhdq xmm1, xmm7  ; [ 0 |  0 | 0 | b3 | 0 | 0 | 0 |  r3 | 0 | 0 | 0 |  g3 | 0 | 0 | 0 | a3 ] 
-	punpckldq xmm3, xmm7   ; [ 0 |  0 | 0 | b4 | 0 | 0 | 0 |  r4 | 0 | 0 | 0 |  g4 | 0 | 0 | 0 | a4 ] 
-	;posicion en convolucion
+	punpckhwd xmm0, xmm7  ; [ 0 |  0 | 0 | b1 | 0 | 0 | 0 |  r1 | 0 | 0 | 0 |  g1 | 0 | 0 | 0 | a1 ] 
+	punpcklwd xmm2, xmm7   ; [ 0 |  0 | 0 | b2 | 0 | 0 | 0 |  r2 | 0 | 0 | 0 |  g2 | 0 | 0 | 0 | a2 ] 
+	punpckhwd xmm1, xmm7  ; [ 0 |  0 | 0 | b3 | 0 | 0 | 0 |  r3 | 0 | 0 | 0 |  g3 | 0 | 0 | 0 | a3 ] 
+	punpcklwd xmm3, xmm7   ; [ 0 |  0 | 0 | b4 | 0 | 0 | 0 |  r4 | 0 | 0 | 0 |  g4 | 0 | 0 | 0 | a4 ]
+	cvtdq2ps xmm0,xmm0
+	cvtdq2ps xmm1,xmm1
+	cvtdq2ps xmm2,xmm2 
+	cvtdq2ps xmm3,xmm3
+	;posicion en convolucion, voy a usar para levantar 4 floats
 	mov rcx,r8
 	shl rcx,1
 	inc rcx ;diametro
 	mov r12,r9 ;matrix de conv
-	mov rax,r10
+	mov rax,r10 ;posicion en y
 	mul rcx
-	add rax,r11
+	add rax,r11 ;posicion en x
 	shl rax,2
-	add r9,rax
-	movdqu xmm4,[r9]
+	add r12,rax
 	;posicion en convolucion
+	movdqu xmm4,[r12] ;levanto 4 floats
 	pshufd xmm6,xmm4,0 
 	mulps xmm6,xmm0
-	;movdqu xmm1,r9  ;valor conv todavia no tengo conv
  	mov rcx,r8
 	shl rcx,1
 	inc rcx ;diametro
  	;desempaqueto y multiplico
-	addps xmm2,xmm6 ;agrego el primer valor
+	addps xmm5,xmm6 ;agrego el primer valor
 	inc r11 ;incremento puntero en x
 	cmp r11,rcx ;comparo con diametro
 	jne .noLLegoAlDiametroEnElPrimerNodo
 	inc r10 ;incremento puntero a y
 	cmp r10,rcx ;comparo con diametro
 	je .termine
-	xor rax,rax ;agrego un pixel a la matriz de conv
-	inc rax
-	shl rax,2
-	add r9,rax ;agregue un pixel a la matriz conv (proximo)
-	mov rax,rdx ;muevo columnas a r9
-	sub rax,rcx ;le resto el diametro
-	shl rax,2
-	add r12,rax ; voy al proximo valor de la matriz original
 	xor r11,r11
 	jmp .empezarACalcular
 .noLLegoAlDiametroEnElPrimerNodo:	
-	addps xmm2,xmm5 ;agrego el segundo valor
+	pshufd xmm6,xmm4,85 
+	mulps xmm6,xmm1
+	addps xmm5,xmm6 ;agrego el segundo valor
 	inc r11 ;incremento puntero en x
 	cmp r11,rcx ;comparo con diametro
 	jne .noLLegoAlDiametroEnElSegundoNodo
 	inc r10 ;incremento puntero a y
 	cmp r10,rcx ;comparo con diametro
 	je .termine
-	xor rax,rax ;agrego un pixel a la matriz de conv
-	inc rax
-	inc rax
-	shl rax,2
-	add r9,rax ;agregue un dos a la matriz conv (proximo)
-	mov rax,rdx ;muevo columnas a r9
-	sub rax,rcx ;le resto el diametro
-	shl rax,2
-	inc rax 
-	add r12,rax ; voy al proximo valor de la matriz original
 	xor r11,r11
 	jmp .empezarACalcular
 .noLLegoAlDiametroEnElSegundoNodo:
+	pshufd xmm6,xmm4,170
+	mulps xmm6,xmm2
+	addps xmm5,xmm6 ;agrego el segundo valor
 	addps xmm2,xmm6 ;agrego el primer valor
 	inc r11 ;incremento puntero en x
 	cmp r11,rcx ;comparo con diametro
@@ -150,51 +147,27 @@ blur_asm:
 	inc r10 ;incremento puntero a y
 	cmp r10,rcx ;comparo con diametro
 	je .termine
-	xor rax,rax ;agrego tres pixeles a la matriz de conv
-	inc rax
-	inc rax
-	inc rax
-	shl rax,2
-	add r9,rax ;agregue tres pixeles a la matriz conv (proximo)
-	mov rax,rdx ;muevo columnas a r9
-	sub rax,rcx ;le resto el diametro
-	inc rax
-	inc rax ;le agrego 2 pixeles
-	shl rax,2
-	add r12,rax ; voy al proximo valor de la matriz original
 	xor r11,r11
 	jmp .empezarACalcular
 .noLLegoAlDiametroEnElTercerNodo:
-	addps xmm2,xmm7 ;agrego el primer valor
+	pshufd xmm6,xmm4,255
+	mulps xmm6,xmm2
+	addps xmm5,xmm6 ;agrego el segundo valor
 	inc r11 ;incremento puntero en x
 	cmp r11,rcx ;comparo con diametro
-	jne .noLLegoAlDiametroEnElCuartoNodo
+	jne .empezarACalcular
 	inc r10 ;incremento puntero a y
 	cmp r10,rcx ;comparo con diametro
 	je .termine
-	xor rax,rax ;agrego cuatro pixel a la matriz de conv
-	inc rax
-	shl rax,4
-	add r9,rax ;agregue cuatro pixeles a la matriz conv (proximo)
-	mov rax,rdx ;muevo columnas a r9
-	sub rax,r13 ;le resto el diametro
-	inc rax
-	inc rax
-	inc rax
-	shl rax,2
-	add r12,rax ; voy al proximo valor de la matriz original
 	xor r11,r11
-	jmp .empezarACalcular
-.noLLegoAlDiametroEnElCuartoNodo:
-	xor rax,rax ;agrego cuatro pixel a la matriz de conv
-	inc rax
-	shl rax,4
-	add r9,rax ;agregue cuatro pixeles a la matriz conv (proximo)
-	add r12,rax
 	jmp .empezarACalcular
 .termine:
 	;pixel(punteroOriginalX,punteroOriginalY) = contadores
 	;r14 = y r15 = x
+	CVTTPS2DQ xmm5,xmm5
+	packusdw xmm5,xmm5
+	packuswb xmm5,xmm5
+	pextrd r10d,xmm5,0
 	xor rax,rax
 	mov eax,ebx ;columnas
 	mov rcx,r14 ;posicion en y
@@ -206,9 +179,9 @@ blur_asm:
 	add r11,rax
 	add r12,rax
 	;----------------;
-	;mov r10d,[r11]
-	xor r10,r10
 	mov [r12],r10d
+	xor rax,rax
+	mov [r12+3],al
 	;----------------;
 	inc r15 ;puntero a x
 	mov rax,r13 ;columnas
