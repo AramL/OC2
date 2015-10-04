19f812adc58d3f2f3d2eb9754b60824bce9cc7be
AramL <leandrojavr@gmail.com>
2015-10-04 06:11:09 -0300

    mas legible

--- TP2/filtros/blur_c.c
+++ TP2/filtros/blur_c.c
@@ -7,15 +7,15 @@
 #define pi 3.14159265359
 
 float* matrizDeConvolucion(float sigma, int radius){ 
-
+    int width = radius * 2 + 1;
     float den = 2* pi * sigma * sigma;
     float in_den = (-2) * sigma * sigma;
-    float *convolutionMatrix = (float *) malloc(sizeof(float) * ((2*radius)+1) * ((2*radius)+1));
+    float *convolutionMatrix = (float *) malloc(sizeof(float) * width * width);
     for(int h = -radius; h <= radius; h++){
         for(int p = -radius; p <= radius; p++){
             int x = (radius - h)*(radius - h);
             int y = (radius - p)*(radius - p);
-            convolutionMatrix[(h+radius)*(2*radius+1) + p+radius] = exp((x+y)/in_den)/den;
+            convolutionMatrix[(h+radius)*(width) + p + radius] = exp((x+y)/in_den)/den;
         }    
     }
     return convolutionMatrix;
@@ -27,14 +27,10 @@ void blur_c  (unsigned char *src, unsigned char *dst, int cols, int filas, float
     unsigned char (*dst_matrix)[cols * 4] = (unsigned char (*)[cols * 4]) dst;
 
     float sigmasq = sigma * sigma;
-
-    float *convolutionMatrix = matrizDeConvolucion(sigma, radius);/* (float *) malloc(sizeof(float) * ((2 * radius) + 1) * ((2 * radius) + 1) ); */
-    //Aca antes estaba unsigned char y se volvia todo loco, PONERLO EN EL INFORMEEEEE!#!!!!!#$!"&$!"=/(#)=
-    /*for (int h = -radius; h <= radius; h++)
-        for (int p = -radius; p <= radius; p++)
-            convolutionMatrix[(h + radius) * (2 * radius + 1) + p + radius] = exp((((radius - h) * (radius - h)) + ((radius - p) * (radius - p))) / ((-2) * sigmasq)) / (2 * pi * sigmasq);
-    */
-
+    int width = radius * 2 +1;
+    
+    float (*convolutionMatrix)[width] = (float (*)[width])matrizDeConvolucion(sigma, radius);
+    
     float tempB = 0;
     float tempR = 0;
     float tempG = 0;
@@ -47,9 +43,9 @@ void blur_c  (unsigned char *src, unsigned char *dst, int cols, int filas, float
 
             for (int j = -radius; j <= radius; j++) {
                 for (int k = -radius; k <= radius; k++) {
-                    tempB += src_matrix[y + j][(x + k) * 4]   * convolutionMatrix[(j + radius) * (2 * radius + 1) + k + radius];
-                    tempR += src_matrix[y + j][(x + k) * 4 + 1] * convolutionMatrix[(j + radius) * (2 * radius + 1) + k + radius];
-                    tempG += src_matrix[y + j][(x + k) * 4 + 2] * convolutionMatrix[(j + radius) * (2 * radius + 1) + k + radius];
+                    tempB += src_matrix[y + j][(x + k) * 4]   * convolutionMatrix[j+radius][k+radius];
+                    tempR += src_matrix[y + j][(x + k) * 4 + 1] * convolutionMatrix[j+radius][k+radius];
+                    tempG += src_matrix[y + j][(x + k) * 4 + 2] * convolutionMatrix[j+radius][k+radius];
                 }
 
             }
