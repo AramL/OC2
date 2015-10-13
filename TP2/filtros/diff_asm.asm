a2db4481ad42f00a2fc562eb372058abd4bb2ee9
uriel <urielrozenberg@hotmail.com>
2015-10-13 19:45:23 -0300

    mascarah

--- TP2/filtros/diff_asm.asm
+++ TP2/filtros/diff_asm.asm
@@ -6,8 +6,8 @@ extern printf
 
 section .rodata:
 ALIGN 16
-mask_5  db 2,2,2,2,6,6,6,6,10,10,10,10,14,14,14,14
-trans_2 db 0,0,0,255,0,0,0,255,0,0,0,255,0,0,0,255
+mask db 3,3,3,3,7,7,7,7,11,11,11,11,15,15,15,15
+trans db 0,0,0,255,0,0,0,255,0,0,0,255,0,0,0,255
 es_int  db "%c= %i",10,0
 
 
@@ -54,10 +54,10 @@ xor r15, r15
     pslldq xmm4, 1                  ;xmm4= [-G0-|B0|A0|R1|-G1-|B1|A1|R2|-G2-|B2|A2|R3|-G3-|B3|A3|0]_0
     pslldq xmm5, 2                  ;xmm5= [-B0-|A0|R1|G1|-B1-|A1|R2|G2|-B2-|A2|R3|G3|-B3-|A3|0|0]_0
     movdqu xmm6, xmm5               ;xmm6= [-B0-|A0|R1|G1|-B1-|A1|R2|G2|-B2-|A2|R3|G3|-B3-|A3|0|0]_0
-    pmaxub xmm6, xmm4               ;xmm6=[max(R0,G0)|@@|@@|@@|max(R1,G1)|@@|@@|@@|max(R2,G2)|@@|@@|@@|max(R3,G3)|@@|@@|@@]_0      @@ = basura
+    pmaxub xmm6, xmm4               ;xmm6=(127)15_[max(R0,G0)|@@|@@|@@|max(R1,G1)|@@|@@|@@|max(R2,G2)|@@|@@|@@|max(R3,G3)|@@|@@|@@]_0      @@ = basura
     pmaxub xmm6, xmm3               ;xmm6=[max(R0,G0,B0)|+@@|+@@|+@@|max(R1,G1,B1)|+@@|+@@|+@@|max(R2,G2,B2)|+@@|+@@|+@@|max(R3,G3,B3)|+@@|+@@|+@@]_0  +@@ = màs basura :D
-    pshufb xmm6, [mask_5] 
-    paddsb xmm6, [trans_2]
+    pshufb xmm6, [mask] 
+    paddsb xmm6, [trans]
     movdqu [r12 +  r15*4], xmm6
     add  r15d, 4
     jmp .ciclo
