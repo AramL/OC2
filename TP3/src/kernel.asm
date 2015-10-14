876739c9bb6b81efb3aa185b8c26b2768617486c
AramL <leandrojavr@gmail.com>
2015-10-14 01:06:43 -0300

    kernel un par de comentarios boludos

--- TP3/src/kernel.asm
+++ TP3/src/kernel.asm
@@ -20,6 +20,12 @@ iniciando_mr_len equ    $ - iniciando_mr_msg
 iniciando_mp_msg db     'Iniciando kernel (Modo Protegido)...'
 iniciando_mp_len equ    $ - iniciando_mp_msg
 
+bienvenida_mp_msg db    'Bienvenido!!!!111'
+bienvenida_mp_len equ   $ - bienvenida_mp_msg
+
+inicializando_mp_msg db    'Inicializando el Juego'
+inicializando_mp_len equ   $ - inicializando_mp_msg
+
 ;;
 ;; Seccion de código.
 ;; -------------------------------------------------------------------------- ;;
@@ -40,20 +46,27 @@ start:
     ; Imprimir mensaje de bienvenida
     imprimir_texto_mr iniciando_mr_msg, iniciando_mr_len, 0x07, 0, 0
 
-
     ; Habilitar A20
     call habilitar_A20
     ; Cargar la GDT
     lgdt  [GDT_DESC]
     ; Setear el bit PE del registro CR0
-    mov eax,cr0
+    ; Para activar modo protegido necesitamos setear en 1
+    ; el bit 0(PE = protected enviroment) del registro de control 0
+    mov eax, cr0
     or  eax, 1
     mov cr0, eax
+    ; Estamos casi en condiciones de pasar a modo protegido
     ; Saltar a modo protegido
     jmp 0x40:modoprotegido
-    BITS 32
+    ; Hacemos un jmp far haciendo que CS apunte a nuestra tabla de codigo (gdt.c/h) (8 << 3)
+    ; como al hacer jmp pasamos a modo protegido le tenemos que decir a NASM 
+    ; que empieze a generar código diseñado para un procesador de 32 bits.
+BITS 32
     modoprotegido:
     ; Establecer selectores de segmentos
+    ; Establecemos los sectores, eax apunta al valor de la primera tabla de datos shifteado por 3
+    ; (10 << 3)
     mov eax, 0x50
     mov ss, ax
     mov ds, ax
@@ -61,13 +74,15 @@ start:
     mov fs, ax
     mov es, ax
     ; Establecer la base de la pila
-    mov ebp, 0x28000
+    ; esp indica donde empieza la pila.
+    mov ebp, 0x1337
     mov esp, 0x28000
-
+    ;Mensaje de inicio modo protegido
+    imprimir_texto_mp  iniciando_mp_msg, iniciando_mp_len, 0x07, 2, 0
     ; Imprimir mensaje de bienvenida
-    imprimir_texto_mp  iniciando_mp_msg, iniciando_mp_len, 0x07, 0, 0
+    imprimir_texto_mp  bienvenida_mp_msg, bienvenida_mp_len, 0x07, 3, 0
     ; Inicializar el juego
-
+    imprimir_texto_mp  inicializando_mp_msg, inicializando_mp_len, 0x07, 4, 0
     ; Inicializar pantalla
 
     ; Inicializar el manejador de memoria
