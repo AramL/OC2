e2a61a45ff7ebc28dfea8426586093e91637eb2f
AramL <leandrojavr@gmail.com>
2015-10-21 21:47:08 -0300

    mmu.c

--- TP3/src/mmu.c
+++ TP3/src/mmu.c
@@ -7,8 +7,24 @@
 
 #include "mmu.h"
 #include "i386.h"
+
+uint proxima_pagina_libre = 0x100000;
+
 /* Atributos paginas */
 /* -------------------------------------------------------------------------- */
+uint mmu_proxima_pagina_fisica_libre(){
+    uint ret = proxima_pagina_libre;
+    proxima_pagina_libre += 0x1000;
+    return ret;
+}
+
+void mmu_mapear_pagina  (uint virtual, uint cr3, uint fisica, uint attrs){
+
+}
+
+uint mmu_unmapear_pagina(uint virtual, uint cr3){
+
+}
 
 
 /* Direcciones fisicas de codigos */
