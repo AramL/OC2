856e4faceb18dca403e68d9835425a2de93f1c42
AramL <leandrojavr@gmail.com>
2015-11-01 00:12:45 -0300

    dif

--- TP2/filtros/diff_asm.asm
+++ TP2/filtros/diff_asm.asm
@@ -18,53 +18,53 @@ section .text
 
 _diff_asm:
 diff_asm:
-push rbp                 
-mov rbp, rsp
-push r12
-push r13
-push r14
-sub rsp, 8
+    push rbp                 
+    mov rbp, rsp
+    push r12
+    push r13
+    push r14
+    sub rsp, 8
 
-mov r12, rdx
-;Mover registros de 32 bits (esto solo vale para 32 bits, con 16 y 8 no!), limpia los bits mas altos
-;entonces al hacer mul rcx, tendriamos el resultado en rdx:rax, pero como los 32 mas altos de rax y rcx estan en 0,
-;el resultado que queremos, osea la multiplicación esta en rax (reg de 32bits x reg de 32bits
-;entra en 64bits). Esto nos ahorra hacer un shift de 32b y un add.
-mov eax, r8d
-mov ecx, ecx
-mul rcx
-;shl rdx, 32
-;add rax, rdx
-xor r14, r14
-.ciclo:
-    cmp r14, rax
-    JE .fin
+    mov r12, rdx
+    ;Mover registros de 32 bits (esto solo vale para 32 bits, con 16 y 8 no!), limpia los bits mas altos
+    ;entonces al hacer mul rcx, tendriamos el resultado en rdx:rax, pero como los 32 mas altos de rax y rcx estan en 0,
+    ;el resultado que queremos, osea la multiplicación esta en rax (reg de 32bits x reg de 32bits
+    ;entra en 64bits). Esto nos ahorra hacer un shift de 32b y un add.
+    mov eax, r8d
+    mov ecx, ecx
+    mul rcx
+    ;shl rdx, 32
+    ;add rax, rdx
+    xor r14, r14
+    .ciclo:
+        cmp r14, rax
+        JE .fin
 
-    movdqu xmm3 , [rdi +  r14*4]    ;xmm3= [-R0-|G0|B0|A0|-R1-|G1|B1|A1|-R2-|G2|B2|A2|-R3-|G3|B3|A3]_0  
-    movdqu xmm15, [rsi +  r14*4]    ;xmm15= [-R0-|G0|B0|A0|-R1-|G1|B1|A1|-R2-|G2|B2|A2|-R3-|G3|B3|A3]_0 de la segunda immagen
-    movdqu xmm14, xmm15             ; back upeo xmm15 para usar ese valor despues
-    pminub xmm15, xmm3              ;aca tiene minimo entre R_src1 y R_src2 para cada uno
-    pmaxub xmm3 , xmm14             ; aca el maximo
-    psubb  xmm3 , xmm15              ;max(a,b)    - min(a,b) = mod(a-b)
-                                    ;xmm3=[]
-    movdqu xmm4, xmm3 
-    movdqu xmm5, xmm3       
-    pslldq xmm4, 1                  ;xmm4= [-G0-|B0|A0|R1|-G1-|B1|A1|R2|-G2-|B2|A2|R3|-G3-|B3|A3|0]_0
-    pslldq xmm5, 2                  ;xmm5= [-B0-|A0|R1|G1|-B1-|A1|R2|G2|-B2-|A2|R3|G3|-B3-|A3|0|0]_0
-    movdqu xmm6, xmm5               ;xmm6= [-B0-|A0|R1|G1|-B1-|A1|R2|G2|-B2-|A2|R3|G3|-B3-|A3|0|0]_0
-    pmaxub xmm6, xmm4               ;xmm6=(127)15_[max(R0,G0)|@@|@@|@@|max(R1,G1)|@@|@@|@@|max(R2,G2)|@@|@@|@@|max(R3,G3)|@@|@@|@@]_0      @@ = basura
-    pmaxub xmm6, xmm3               ;xmm6=[max(R0,G0,B0)|+@@|+@@|+@@|max(R1,G1,B1)|+@@|+@@|+@@|max(R2,G2,B2)|+@@|+@@|+@@|max(R3,G3,B3)|+@@|+@@|+@@]_0  +@@ = màs basura :D
-    pshufb xmm6, [mask] 
-    paddsb xmm6, [trans]
-    movdqu [r12 +  r14*4], xmm6
-    add  r14d, 4
-    jmp .ciclo
+        movdqu xmm3 , [rdi +  r14*4]    ;xmm3= [-R0-|G0|B0|A0|-R1-|G1|B1|A1|-R2-|G2|B2|A2|-R3-|G3|B3|A3]_0  
+        movdqu xmm15, [rsi +  r14*4]    ;xmm15= [-R0-|G0|B0|A0|-R1-|G1|B1|A1|-R2-|G2|B2|A2|-R3-|G3|B3|A3]_0 de la segunda immagen
+        movdqu xmm14, xmm15             ; back upeo xmm15 para usar ese valor despues
+        pminub xmm15, xmm3              ;aca tiene minimo entre R_src1 y R_src2 para cada uno
+        pmaxub xmm3 , xmm14             ; aca el maximo
+        psubb  xmm3 , xmm15              ;max(a,b)    - min(a,b) = mod(a-b)
+                                        ;xmm3=[]
+        movdqu xmm4, xmm3 
+        movdqu xmm5, xmm3       
+        pslldq xmm4, 1                  ;xmm4= [-G0-|B0|A0|R1|-G1-|B1|A1|R2|-G2-|B2|A2|R3|-G3-|B3|A3|0]_0
+        pslldq xmm5, 2                  ;xmm5= [-B0-|A0|R1|G1|-B1-|A1|R2|G2|-B2-|A2|R3|G3|-B3-|A3|0|0]_0
+        movdqu xmm6, xmm5               ;xmm6= [-B0-|A0|R1|G1|-B1-|A1|R2|G2|-B2-|A2|R3|G3|-B3-|A3|0|0]_0
+        pmaxub xmm6, xmm4               ;xmm6=(127)15_[max(R0,G0)|@@|@@|@@|max(R1,G1)|@@|@@|@@|max(R2,G2)|@@|@@|@@|max(R3,G3)|@@|@@|@@]_0      @@ = basura
+        pmaxub xmm6, xmm3               ;xmm6=[max(R0,G0,B0)|+@@|+@@|+@@|max(R1,G1,B1)|+@@|+@@|+@@|max(R2,G2,B2)|+@@|+@@|+@@|max(R3,G3,B3)|+@@|+@@|+@@]_0  +@@ = màs basura :D
+        pshufb xmm6, [mask] 
+        paddsb xmm6, [trans]
+        movdqu [r12 +  r14*4], xmm6
+        add  r14d, 4
+        jmp .ciclo
 
-.fin:
+    .fin:
 
-add rsp, 8
-pop r14
-pop r13
-pop r12
-pop rbp
-ret
+    add rsp, 8
+    pop r14
+    pop r13
+    pop r12
+    pop rbp
+    ret
---
+++ TP2/filtros/diff_asm_no_SIMD_1pixel.asm
@@ -0,0 +1,118 @@
+;default rel
+global _diff_asm
+global diff_asm
+
+section .text
+
+
+; r12w = x_1
+; r13w = y_1
+; r14w = z_1
+; rbp  = x_2
+; r8   = y_2
+; r9   = z_3
+
+;void diff_asm    (
+    ;unsigned char *src,       ;rdi
+    ;unsigned char *src2,      ;rsi
+    ;unsigned char *dst,       ;rdx
+    ;int filas,                ;ecx
+    ;int cols)                 ;r8d
+_diff_asm:
+diff_asm:
+    push rbp
+    push r12
+    push r13
+    push r14
+    push r15
+    push rbx
+    sub rsp, 8
+
+    mov r15, rdx
+    mov eax, r8d
+    mov ecx, ecx
+    mul rcx
+    mov rcx, rax
+    mov rdx, r15
+
+    .ciclo:
+    cmp rcx, 0
+    je .fin
+
+
+    mov ebx, [rdi + rcx*4 - 4]
+    movzx r12, bl
+    shr rbx, 8
+    movzx r13, bl
+    shr rbx, 8
+    movzx r14, bl
+
+    mov r15d, [rsi + rcx*4 - 4]
+    movzx rbp, r15b
+    shr r15, 8
+    movzx r8, r15b
+    shr r15, 8
+    movzx r9, r15b
+
+    ;call inf_norm 
+
+
+    ;Calculo los valores absolutos
+    sub r12d, ebp
+    mov ebp, r12d
+    sar ebp, 0x1f
+    xor r12d, ebp
+    sub r12d, ebp
+
+    sub r13d, r8d
+    mov r8d, r13d
+    sar r8d, 0x1f
+    xor r13d, r8d
+    sub r13d, r8d
+
+    sub r14d, r9d
+    mov r9d, r14d
+    sar r9d, 0x1f
+    xor r14d, r9d
+    sub r14d, r9d
+
+    cmp r12w, r13w
+    jg .z_y
+    cmp r13w, r14w
+    cmovl r13d, r14d
+    jmp .insert
+
+    .z_y:
+    cmp r12w, r14w
+    mov r13d, r14d
+    cmovge r13d, r12d
+
+    .insert:
+
+    mov al, 0
+    shl rax, 8
+    mov al, r13b
+    shl rax, 8
+    mov al, r13b
+    shl rax, 8
+    mov al, r13b
+
+
+    mov [rdx + rcx*4 - 4], eax
+
+    dec rcx
+    
+    jmp .ciclo
+
+    .fin:
+
+    add rsp, 8
+    pop rbx
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop rbp
+    ret
+
+
