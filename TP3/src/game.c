f47d7733d68b50b8a0a2b388373454873e868c74
AramL <leandrojavr@gmail.com>
2015-12-15 00:49:36 -0300

    s

--- TP3/src/game.c
+++ TP3/src/game.c
@@ -13,8 +13,8 @@ TRABAJO PRACTICO 3 - System Programming - ORGANIZACION DE COMPUTADOR II - FCEN
 
 
 int escondites[ESCONDITES_CANTIDAD][3] = { // TRIPLAS DE LA FORMA (X, Y, HUESOS)
-	{76,  25, 50}, {12, 15, 50}, {9, 10, 100}, {47, 21, 100} ,
-	{34,  11, 50}, {75, 38, 50}, {40, 21, 100}, {72, 17, 100}
+    {76,  25, 50}, {12, 15, 50}, {9, 10, 100}, {47, 21, 100} ,
+    {34,  11, 50}, {75, 38, 50}, {40, 21, 100}, {72, 17, 100}
 };
 
 jugador_t jugadorA;
@@ -24,43 +24,43 @@ perro_t *game_perro_actual = NULL;
 int ultimo_cambio = MAX_SIN_CAMBIOS;
 
 void ASSERT_OR_ERROR(uint value, char* error_msg) {
-	if (!value) {
-		print(error_msg, 5, 5, C_BG_LIGHT_GREY | C_FG_BLACK);
-		breakpoint();
-	}
+    if (!value) {
+        print(error_msg, 5, 5, C_BG_LIGHT_GREY | C_FG_BLACK);
+        breakpoint();
+    }
 }
 
 void* error() {
-	__asm__ ("int3");
-	return 0;
+    __asm__ ("int3");
+    return 0;
 }
 
 uint game_xy2lineal (uint x, uint y) {
-	return (y * MAPA_ANCHO + x);
+    return (y * MAPA_ANCHO + x);
 }
 
 uint game_es_posicion_valida(int x, int y) {
-	return (x >= 0 && y >= 0 && x < MAPA_ANCHO && y < MAPA_ALTO);
+    return (x >= 0 && y >= 0 && x < MAPA_ANCHO && y < MAPA_ALTO);
 }
 
 
 void game_inicializar() {
-	game_jugador_inicializar(&jugadorA);
-	game_jugador_inicializar(&jugadorB);
+    game_jugador_inicializar(&jugadorA);
+    game_jugador_inicializar(&jugadorB);
 
-	screen_pintar_puntajes();
+    screen_pintar_puntajes();
 }
 
 
 // devuelve la cantidad de huesos que hay en la posición pasada como parametro
 uint game_huesos_en_posicion(uint x, uint y) {
-	int i;
-	for (i = 0; i < ESCONDITES_CANTIDAD; i++)
-	{
-		if (escondites[i][0] == x && escondites[i][1] == y)
-			return escondites[i][2];
-	}
-	return 0;
+    int i;
+    for (i = 0; i < ESCONDITES_CANTIDAD; i++)
+    {
+        if (escondites[i][0] == x && escondites[i][1] == y)
+            return escondites[i][2];
+    }
+    return 0;
 }
 
 
@@ -68,20 +68,20 @@ uint game_huesos_en_posicion(uint x, uint y) {
 
 // devuelve algun perro que esté en la posicion pasada (hay max 2, uno por jugador)
 perro_t* game_perro_en_posicion(uint x, uint y) {
-	int i;
-	for (i = 0; i < MAX_CANT_PERROS_VIVOS; i++)
-	{
-		if (!jugadorA.perros[i].libre && jugadorA.perros[i].x == x && jugadorA.perros[i].y == y)
-			return &jugadorA.perros[i];
-		if (!jugadorB.perros[i].libre && jugadorB.perros[i].x == x && jugadorB.perros[i].y == y)
-			return &jugadorB.perros[i];
-	}
-	return NULL;
+    int i;
+    for (i = 0; i < MAX_CANT_PERROS_VIVOS; i++)
+    {
+        if (!jugadorA.perros[i].libre && jugadorA.perros[i].x == x && jugadorA.perros[i].y == y)
+            return &jugadorA.perros[i];
+        if (!jugadorB.perros[i].libre && jugadorB.perros[i].x == x && jugadorB.perros[i].y == y)
+            return &jugadorB.perros[i];
+    }
+    return NULL;
 }
 
 
 uint game_perro_mover(perro_t *perro, direccion dir) {
-	return 0;
+    return 0;
 }
 
 
@@ -92,34 +92,34 @@ uint game_perro_cavar(perro_t *perro){
 }
 
 uint game_perro_olfatear(perro_t *perro) {
-	return 0;
+    return 0;
 }
 
 
 // termina si se agotaron los huesos o si hace tiempo que no hay ningun cambio
 void game_terminar_si_es_hora() {
-	int i;
-	uint hay_huesos = FALSE;
-	for (i = 0; i < ESCONDITES_CANTIDAD; i++)
-	{
-		if (escondites[i][2] == 0) continue;
-		hay_huesos = TRUE;
-		break;
-	}
-	if (!hay_huesos || ultimo_cambio == 0) {
-		if (jugadorA->puntos > jugadorB->puntos) {
-			screen_stop_game_show_winner(&jugadorA);
-		} else {
-			screen_stop_game_show_winner(&jugadorB);
-		}
-	}
+    int i;
+    uint hay_huesos = FALSE;
+    for (i = 0; i < ESCONDITES_CANTIDAD; i++)
+    {
+        if (escondites[i][2] == 0) continue;
+        hay_huesos = TRUE;
+        break;
+    }
+    if (!hay_huesos || ultimo_cambio == 0) {
+        if (jugadorA->puntos > jugadorB->puntos) {
+            screen_stop_game_show_winner(&jugadorA);
+        } else {
+            screen_stop_game_show_winner(&jugadorB);
+        }
+    }
 }
 
 
 
 
 void dame_un_perro_laputamadre() {
-	llenar_descriptor_tss_perro(0, &jugadorA.perros[0], 0, 0);
+    llenar_descriptor_tss_perro(0, &jugadorA.perros[0], 0, 0);
 }
 
 
